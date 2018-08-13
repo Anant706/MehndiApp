5e3bb98f36ad77ab8c20a949e4c5d6a3bb86ecd7
Anant706 <infinite.yadav@gmail.com>
2018-08-13 10:46:46 -0500

    Update AppConst.java

--- MyDesign/app/src/main/java/app/AppConst.java
+++ MyDesign/app/src/main/java/app/AppConst.java
@@ -17,39 +17,11 @@ public class AppConst {
     public static final String SDCARD_DIR_NAME = "Mehndi Wallpaper";
 
     // Picasa/Google web album username
-    public static final String PICASA_USER = "ashok6275";
+    public static final String PICASA_USER = "_______";
 
-    public static final String URL_PICASA_ALBUMS = "https://picasaweb.google" +
-            ".com/data/feed/api/user/ashok6275?kind=album&alt=json";
+    public static final String URL_TATOO_M = "_______________________";
 
-    public static final String URL_TATOO_M = "https://picasaweb.google.com/data/feed/api/user/infinite.yadav/albumid/6176439105725001521?kind=photo&alt=json";
-
-    public static final String URL_HAND_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6189937858923029569?alt=json";
-    public static final String URL_LEG_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6189938634580557089?kind=photo&alt=json";
-    public static final String URL_TEEJ_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6189947023849951009?kind=photo&alt=json";
-    public static final String URL_ANNIVER_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6188340515886141425?kind=photo&alt=json";
-    public static final String URL_KADAWA_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6189947293927902929?kind=photo&alt=json";
-
-    public static final String URL_WEDDING_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6189945826783712945?kind=photo&alt=json";
-
-    public static final String URL_VINTAGE_INDIAN_JEWEL = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6290121198650286753?kind=photo&alt=json";
-
-    public static final String URL_DULHAN_DRESS_COLLECTION = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6290120190489736689?kind=photo&alt=json";
-
-    public static final String URL_FANCY_DRESS_COLLECTION = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6290120711069439857?kind=photo&alt=json";
-
-    public static final String URL_EYE_BRO_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6290119623734052257?kind=photo&alt=json";
-
-    public static final String URL_FANCY_NAIL = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6204031085458485249?kind=photo&alt=json";
-
-    public static final String URL_LIPS_SHADES = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6290119909424137073?kind=photo&alt=json";
-
-    public static final String URL_HAIR_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6191224334215275457?kind=photo&alt=json";
-
-    public static final String URL_RANGOLI_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6277014297183628001?kind=photo&alt=json";
-
-    public static final String URL_MIX_M = "https://picasaweb.google.com/data/feed/api/user/ashok6275/albumid/6189942343051088449?kind=photo&alt=json";
-
+ 
     ////////////////////////////
 
 }
